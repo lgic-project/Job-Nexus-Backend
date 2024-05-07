ee6fd136c0be86fce3eac4c0d2824fd4a8803afb
Binaya9898 <82639575+Binaya9898@users.noreply.github.com>
2024-05-07 08:48:28 +0545

    Job Details Section Done

--- resources/views/admin/modules/jobs/listjob.blade.php
+++ resources/views/admin/modules/jobs/listjob.blade.php
@@ -71,12 +71,87 @@
                         Delete
                     </button>
                 </a>
-                <a href="/job/profile/{{$jobData->id}}">
 
-                    <button type="button" class="btn btn-warning btn-sm btn-rounded mx-2 px-2">
-                        Details
-                    </button>
-                </a>
+
+                <button type="button" class="btn btn-warning btn-sm btn-rounded mx-2 px-2" data-bs-toggle="modal" data-bs-target="#resumeModal">
+                    Details
+                </button>
+
+                <div class="modal fade" id="resumeModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Job Details</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="container">
+
+                                    <table class="table">
+                                        <tbody>
+                                            <tr>
+                                                <th>Title</th>
+                                                <td>{{ $jobData->job_title }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Category</th>
+                                                <td>{{ $jobData->job_category }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Address</th>
+                                                <td>{{ $jobData->job_address }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Company Name</th>
+                                                <td>{{ $jobData->job_company_name }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Experience</th>
+                                                <td>{{ $jobData->job_experience }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Salary</th>
+                                                <td>{{ $jobData->job_min_salary }} - {{ $jobData->job_max_salary }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Slug</th>
+                                                <td>{{ $jobData->job_slug }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Status</th>
+                                                <td>{{ $jobData->job_status }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Contact</th>
+                                                <td>{{ $jobData->job_contact }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Validity</th>
+                                                <td>{{ $jobData->job_validity }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Description</th>
+                                                <td>{{ $jobData->job_description }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Type</th>
+                                                <td>{{ $jobData->job_type }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Hour</th>
+                                                <td>{{ $jobData->job_hour }}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </td>
         </tr>
         @endforeach
